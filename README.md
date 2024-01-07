7b700196d3de1d6d14b7e8bd29cfb1e25218ed91
Loongphy <Loongphy@outlook.com>
2024-01-07 21:00:13 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Auth-Server
+
+(WIP) Opinionated OAuth/OIDC Server powered by [Spring Authorization Server](https://spring.io/projects/spring-authorization-server
+).
+
+## Road map
+
+- [ ] [PKCE](https://www.rfc-editor.org/rfc/rfc7636) support
+- [ ] Persistent storage
+- [ ] SPA UI
---
+++ server/src/main/java/com/loongphy/auth/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.loongphy.auth.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
